22d18a3ea4d8341651b01313d6516b1be86aece9
Taewankim <keemtaewan@gmail.com>
2024-09-27 14:23:37 -0700

    update example and readme

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # quadratic-programming-ipm-C
+## Installation Guide
+### Build with Make
+```
+make
+```
+
+### Running the Main file
+The main file includes an example of a quadrtic programming problem. You can run it using the following command:
+```
+./main
+```
+You will see an output similar to this
+```
+iter      objv          gap         |Ax-b|      |Gx+s-h|     step
+------------------------------------------------------------------
+  0     2.528e-02     3.91e+00     9.89e-08     2.07e-01    0.9854
+  1     5.847e-02     5.37e-01     1.00e-07     2.07e-03    0.9900
+  2     4.356e-02     3.07e-02     2.92e-08     9.45e-05    0.9542
+  3     3.802e-02     1.79e-03     5.42e-08     8.88e-06    0.9747
+  4     3.736e-02     9.03e-05     6.29e-09     1.13e-06    0.9853
+  5     3.731e-02     5.34e-06     7.87e-10     1.35e-07    0.9886
+  6     3.731e-02     2.94e-07     1.07e-10     2.06e-08    0.9900
+Converged?: true
+```
+
 ## Reference
- - https://github.com/govindchari/QPSolver
+ - CVXGEN: [Link to paper](https://web.stanford.edu/~boyd/papers/pdf/code_gen_impl.pdf)
+ - Govind's QP solver with Eigen library: [Github Repository](https://github.com/govindchari/QPSolver)
--- main.c
+++ main.c
@@ -1,9 +1,53 @@
 #include <stdio.h>
-#include "matrix.h"
+#include "QPIPM.h"
 
 // int main(int argc, char **argv){
-#define N 5
 int main(){
-    printf("This is main\n");
+    int n = 200;
+    double gamma = 0.5;
+
+    // cost
+    Matrix* Q_ = create_random_matrix(n,n,-1,1);
+    Matrix* Q_trans = transpose(Q_);
+    Matrix* Q = multiply_matrices(Q_,Q_trans);
+    Matrix* Imat = create_identity_matrix(n);
+    add_matrices_with_multiplier_r(Q,Imat,n);
+    multiply_with_scalar_r(Q,gamma);
+    Matrix* q = create_random_matrix(n,1,-1,1);
+
+    // eq
+    Matrix* A = create_matrix(1,n);
+    set_all(A,1.0);
+    Matrix* b = create_matrix(1,1);
+    set_all(b,1.0);
+
+    // ineq
+    Matrix* G = create_identity_matrix(n);
+    multiply_with_scalar_r(G,-1.0);
+    Matrix* h = create_matrix(n,1);
+    set_all(h,0.0);
+
+    // Solve
+    IPM* ipm = create_ipm(Q,q,A,b,G,h);
+    solve_ipm(ipm,true);
+
+    // Check
+    printf("Converged?: %s\n",ipm->converged == true ? "true" : "false");
+    // if (ipm->converged){
+    //     printf("Optimal solution: \n");
+    //     print_solution(ipm->solution);
+    // }
+
+    free_ipm(ipm);
+    free_matrix(A);
+    free_matrix(Q);
+    free_matrix(Q_);
+    free_matrix(Q_trans);
+    free_matrix(Imat);
+    free_matrix(G);
+    free_matrix(q);
+    free_matrix(b);
+    free_matrix(h);
+
     return 0;
 }
